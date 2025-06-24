55d001ec357178ec690b028282ba969c80f890ab
sjh <sjh6226@naver.com>
2025-06-24 11:39:53 +0900

    d

--- react-calculator/src/index.js
+++ react-calculator/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Calculator from './calculator/Calculator';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Calculator />
   </React.StrictMode>
 );
 
